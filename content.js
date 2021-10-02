e3eb88883bbb6a182b1e465e6e956de7789f5036
Khuong <vankhuong240499@gmail.com>
2021-10-03 00:02:09 +0700

    fix Lỗi duplicate class

--- content.js
+++ content.js
@@ -8,7 +8,13 @@ function createElementPage() {
       "style-scope ytd-two-column-browse-results-renderer"
     );
     if (Boolean(id)) {
-      if (id.length > 0) {
+      id_tab_page = document.getElementsByClassName(
+        "style-scope ytd-c4-tabbed-header-renderer iron-selected"
+      );
+
+      if (id_tab_page.length > 0) {
+        // Không làm gì nếu nó content của channel
+      } else if (id.length > 0) {
         id[0].innerHTML = "<h1>Không có nội dung</h1>";
       }
     }
@@ -21,12 +27,32 @@ function createElementPage() {
     }
 
     id_contents = document.getElementById("contents");
-    if (Boolean(id_contents))
-      id_contents.innerHTML = "<h1>Không có nội dung</h1>";
+    if (Boolean(id_contents)) {
+      id_tab_page = document.getElementsByClassName(
+        "style-scope ytd-c4-tabbed-header-renderer iron-selected"
+      );
+
+      if (id_tab_page.length > 0) {
+        // Không làm gì nếu nó content của channel
+      } else {
+        id_contents.innerHTML = "<h1>Không có nội dung</h1>";
+      }
+    }
 
     id_content_zing = document.getElementById("section-latest");
     if (Boolean(id_content_zing)) {
       id_content_zing.innerHTML = "<h1>Không có nội dung</h1>";
+    }
+
+    id_fb_video = document.querySelector('[aria-label="Watch"]');
+    if (Boolean(id_fb_video)) {
+      id_fb_video.style.visibility = "hidden";
+    }
+
+    id_fb_group = document.querySelector('[aria-label="Nhóm"]');
+
+    if (Boolean(id_fb_group)) {
+      id_fb_group.style.visibility = "hidden";
     }
   } catch (error) {}
 }
